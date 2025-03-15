36fc9fd3d5679958dc3ea7fce3295aa932c7c191
Aaditya Shah <aadishah1020@gmail.com>
2025-03-14 21:13:57 -0400

    Enhanced the game with the following improvements:
    Score Functionality – Tracks and displays the player's score.
    Sound Effects – Added audio feedback for button presses and game events.
    Instruction Card – Displays game rules before starting.
    Improved User Experience – Optimized animations and interactions for a smoother gameplay experience.

--- game.js
+++ game.js
@@ -3,6 +3,19 @@ let levels = document.querySelector('.levels');
 let level = 0;
 let boxes = document.querySelectorAll('.box');
 let btn = ['pink', 'green', 'yellow', 'blue'];
+let score = document.querySelector('#score-box');
+let clickAudio = document.querySelector('.click');
+let gameOverAudio = document.querySelector('.gameOver');
+let playBtn = document.querySelector('#play-btn');
+
+playBtn.addEventListener('click', () => {
+    let curtain = document.querySelector('#curtain');
+    curtain.style.transform = "translateY(-100%)";
+    setTimeout(() => {
+        curtain.classList.add("hide");
+    },1000);
+    console.log('button was clicked');
+});
 
 //variables
 let gameSequenceArr = [];
@@ -25,6 +38,7 @@ function startGame() {
         gameStarted = true; //changed the state to started - true
         starter.innerText = '';
         ++level;
+        score.innerText = "Score: " + (level - 1);
         levels.innerText = `Level ${level}`;
         let randomBtnIndex = Math.trunc(Math.random() * 4);
         setTimeout(() => {
@@ -42,12 +56,13 @@ function checkAnswer(index) {
     } 
     else {
         document.body.style.backgroundColor = 'red';
+        gameOverAudio.play();
         for(let box of boxes) {
             box.classList.add('disabled');
         }
         setTimeout(() => {
             document.body.style.backgroundColor = 'white';
-        },500)
+        },1500)
         levels.innerText = `GAME OVER! Your total score was ${level - 1}\nPress any key to restart the game.`;
         gameStarted = false;
         level = 0;
@@ -60,6 +75,7 @@ function levelUp() {
     userClicksArr = [];
     ++level;
     levels.innerText = `Level ${level}`;
+    score.innerText = "Score: " + (level - 1);
     let randomBtnIndex = Math.trunc(Math.random() * 4);
     setTimeout(() => {
         flashButton(boxes[randomBtnIndex]);
@@ -72,6 +88,7 @@ for(let box of boxes) {
 }
 
 function buttonPress() {
+    clickAudio.play();
     userflashButton(this);
     userClicksArr.push(this.classList[2]); //push the color of the box
     checkAnswer(userClicksArr.length - 1);
